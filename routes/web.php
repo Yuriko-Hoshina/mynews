1186f9da2160e07ba6b379e5844e8c327549a16f
Yuriko <utanotantei@gmail.com>
2021-12-06 05:43:33 +0000

    Syuusei

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::group(['prefix' => 'admin'], function(){
 Route::get('XXX', 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/create', 'ProfileController@add');
-    Route::get('profile/edit', 'ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
