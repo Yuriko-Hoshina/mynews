fa828381623523079bbb3a6954d62705fb535684
Yuriko <utanotantei@gmail.com>
2021-12-06 12:06:38 +0000

    Laravel11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,20 @@
         <title>MyNews</title>
     </head>
     <body>
-        <h1>Myニュース作成画面</h1>
+        <h1></h1>
     </body>
 </html>
+
+@extends('layouts.profile')
+
+@section('title', 'My プロフィール')
+
+@section('content')
+  <div class="container">
+      <div class="row">
+          <div class="col-md-8 mx-auto">
+              <h2>My プロフィール</h2>
+          </div>
+      </div>
+  </div>
+@endsection
